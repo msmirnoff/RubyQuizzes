aeb8a985a2e7af0e79a6e209ca3e103b97dc56f4
Michael Smirnoff <michael.smirnoff@sealink.com.au>
2011-11-22 15:54:13 +1030

    updated README. working Quiz 8.

---
+++ Quiz8-RomanNumerals/RomanNumberals/lib/main.rb
@@ -0,0 +1,24 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require './parser.rb'
+
+# Get user input
+print "Roman-Arabic number converter. Please enter a number: "
+input = Parser.new
+
+puts "#{input.value} was entered"
+
+puts input.convert
+
+
+
+
+
+
+
+
+
+
+
+
